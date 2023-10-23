0d9d289b0222a2f8f84f529bb8234968a2152b52
Y0r10 <101215431+Y0r10@users.noreply.github.com>
2023-10-23 16:33:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/App/Routes/route.php";
+require __DIR__ . "/Connections/ConnectionTest.php";
+
+include_once(__DIR__ . "App/Views/home.php");
